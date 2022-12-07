a0b9f1f299c0686b87c814c2b7c551448a61b4c7
nkpe <naomi.pearse@gmail.com>
2022-12-07 14:39:02 +0000

    new note files, with date, Title & Note body

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Python-Notebook
 Using Python to create a notebook style app, running in command line
+
+# To run
+* Download all files. 
+* In terminal cd into folder.
+* Run python index.py in terminal. 
--- index.py
+++ index.py
@@ -1,2 +1,20 @@
-# use file system module to open and edit and close files
+import datetime
 
+def input_note_title():
+    note_title = input("Enter Note Title: ")
+    return note_title
+
+def date_input():
+    date = datetime.datetime.now()
+    return date.strftime('%A %d %B %Y. %H:%M')
+
+def note_book_functionality():
+    note_title = input_note_title()
+    file = open(f'./notes/{note_title}.txt', 'x')
+    file.write(date_input() + '\n')
+    file.write(note_title + '\n')
+    file.write(input("Enter Note: "))
+    file.close()
+    print(f'Note book successfully saved as {note_title}.txt')
+
+note_book_functionality()
